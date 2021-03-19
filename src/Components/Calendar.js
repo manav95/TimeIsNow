789d7ab55c4d3aab720d3f59ccbc86f2bb7b413f
Manav Dutta <Manav.Dutta@capgroup.com>
2021-03-19 16:58:11 -0700

    calendar bar change

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -4,7 +4,8 @@ export default function Calendar({ dark, id }) {
   return (
     <div className={"section" + (dark ? " section-dark" : "")}>
         <div className="section-content" id={id}>
-        <div data-tockify-calendar="spirited" data-tockify-component="calendar"></div>
+        <div data-tockify-component="calendar" data-tockify-calendar="timeisnow"></div>
+<script data-cfasync="false" data-tockify-script="embed" src="https://public.tockify.com/browser/embed.js"></script>
     </div>
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -78,18 +78,6 @@ export default class Navbar extends Component {
                 Join Us
               </Link>
             </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to="section6"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Calendar
-              </Link>
-            </li>
           </ul>
         </div>
       </nav>
