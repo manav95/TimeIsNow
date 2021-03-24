ce288786fa3fd9ac694d7acd6419011c3ca81075
Manav Dutta <Manav.Dutta@capgroup.com>
2021-03-24 10:26:25 -0700

    Updates

---
+++ precache-manifest.bf3b8fe788fa8a5c763b02f5e37fc4a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0c3f4349c987128c6c7",
+    "url": "/TimeIsNow/static/css/main.2fe7f76c.chunk.css"
+  },
+  {
+    "revision": "b0c3f4349c987128c6c7",
+    "url": "/TimeIsNow/static/js/main.b0c3f434.chunk.js"
+  },
+  {
+    "revision": "9496a9e38e5e34ec72b0",
+    "url": "/TimeIsNow/static/js/1.9496a9e3.chunk.js"
+  },
+  {
+    "revision": "376b6ebc42be1594b041",
+    "url": "/TimeIsNow/static/js/runtime~main.376b6ebc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TimeIsNow/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6ecee3672ab9ce7b31c6cdffb7076d9c",
+    "url": "/TimeIsNow/index.html"
+  }
+];
