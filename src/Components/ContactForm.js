aca48d314056e8e27da15811c5984d4f70223849
Manav Dutta <Manav.Dutta@capgroup.com>
2021-03-31 20:12:42 -0700

    contact form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -6,7 +6,7 @@ export default function ContactForm({ title, dark, id, onboardEnabled, onboard }
     <div className={"section" + (dark ? " section-dark" : "")}>
       <div className="section-content" id={id} target="_blank">
            {title} <br></br>
-           {onboardEnabled && <a href="http://tiny.cc/yhnqtz"> <button onClick={onboard} class="onboarding"> Onboarding</button> </a>}
+           {onboardEnabled && <button onClick={onboard} class="onboarding"> Onboarding</button>}
            {!onboardEnabled && <a href="https://discord.gg/SMUAaSUA">Join us on Discord</a>}
            {!onboardEnabled && <Iframe url="https://us7.list-manage.com/contact-form?u=e1e257145a4a30cfd2bf75ef6&form_id=13473e90a944f78f2592a96f9e26121e"
             position="absolute"
